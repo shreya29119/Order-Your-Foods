a252faacd61467b814b87aaa8a5fd2a383c46fa9
Shreya Sharma <shreyasharma29119@gmail.com>
2021-06-16 03:51:39 +0530

    Delete Inkoop directory

--- Inkoop/main.js
+++
@@ -1,8 +0,0 @@
-function classToggle() {
-  const navs = document.querySelectorAll('.nav-items')
-
-  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
-}
-
-document.querySelector('.Navbar__Link-toggle')
-  .addEventListener('click', classToggle);
